13e6c73689ff1396fd5e89bce04ce0048d308c42
vinrain-coder <vincentombogo57@gmail.com>
2024-12-03 01:47:20 +0000

    Complete skills section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Experience />
+      <Skills />
     </div>
   );
 }
