527311eaddfc66ab562a64f98ed3bec30918296e
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-12-06 18:20:53 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -40,7 +40,7 @@ export default function Home() {
     <>
       <Load />
       <Toggle>
-        <Navbar id={id} />
+        <Navbar id={id} className="z-50"/>
         <div ref={compsRef} className="w-full max-w-5xl mx-auto px-1">
           <Hero />
           <About />
